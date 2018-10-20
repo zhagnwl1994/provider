f30b209263d638581e1f67f2bcb6103191bab712
zhangwl <zwlwangy163@163.com>
2018-10-20 22:16:20 +0800

    update

--- src/main/java/org/sang/provider/Controller/HelloController.java
+++ src/main/java/org/sang/provider/Controller/HelloController.java
@@ -6,6 +6,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -16,6 +17,7 @@ public class HelloController {
 
     @Autowired
     private DiscoveryClient discoveryClient;
+    @GetMapping(value = "/hello")
     public String index() {
 
         List<String> services = discoveryClient.getServices();
